36804ec0c14546a0ac769d3dc1b69c724de97418
Mario <mariob360@gmail.com>
2021-09-16 14:24:41 -0700

    Update footer styles far more visible with different background image

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
-import media from 'styled-media-query';
 
 export const FooterWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 40px;
-  color: var(--gray1);
-  font-size: 16px;
+  gap: 1vw;
+  margin: 40px auto;
+  color: var(--gray4);
+  font-size: 12px;
+  letter-spacing: 0.2px;
 
   a {
     font-weight: normal;
     text-decoration: underline;
+    &:hover {
+      color: var(--gray1);
+    }
   }
 `;

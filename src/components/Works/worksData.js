f3d4ff6b94d7edc6a6cde062437c7fbb5df2b66f
Mario <mariob360@gmail.com>
2021-09-30 15:43:02 -0700

    Add personal portfolio data

---
+++ src/components/Works/worksData.js
@@ -0,0 +1,29 @@
+const portfolioData = [
+  {
+    name: `coffeeroasters`,
+    url: 'https://coffee-roasters.vercel.app/',
+    tech: 'NextJS, React, Styled-Components',
+  },
+  {
+    name: `React Colors`,
+    url: 'https://laughing-boyd-cdf70d.netlify.app/',
+    tech: 'React, React-Router, Material-UI',
+  },
+  {
+    name: `Slick's Slices`,
+    url: 'https://slicks-slicess.netlify.app/',
+    tech: 'Gatsby, Sanity, Styled-Components, GraphQL, CSS Variables',
+  },
+  {
+    name: `Weatherman`,
+    url: 'https://weatherman-demo.netlify.app/',
+    tech: 'OpenWeatherMap API, React, Font-Awesome, Styled-Components',
+  },
+  {
+    name: `React Netflix Clone`,
+    url: 'https://flixx-clone.netlify.app/',
+    tech: 'Firebase, React, FuseJS, React-Router',
+  },
+];
+
+export default portfolioData;

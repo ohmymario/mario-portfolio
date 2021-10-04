a6fc5bce6bfb2f7aeba1f2d84ff5b3c48fa809b9
Mario <mariob360@gmail.com>
2021-10-04 16:20:52 -0700

    Fix to match data to images

--- src/components/WorkItems/index.js
+++ src/components/WorkItems/index.js
@@ -23,21 +23,30 @@ const WorkItems = () => {
     }
   `);
 
+  const pics = data.allFile.edges;
+
   return (
     <WorkItem>
-      {data.allFile.edges.map((image, i) => (
-        <a
-          href={portfolioData[i].url}
-          target="_blank"
-          rel="noopener noreferrer"
-          key={i}
-        >
-          <WorkImage
-            imgStyle={{ objectPosition: 'top' }}
-            fluid={image.node.childImageSharp.fluid}
-          />
-        </a>
-      ))}
+      {pics.map((image, i) => {
+        const regex = /[^/]*$/g;
+        const num = image.node.childImageSharp.fluid.src
+          .match(regex)[0]
+          .match(/\d/g, '')[0];
+
+        return (
+          <a
+            href={portfolioData[num - 1].url}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={i}
+          >
+            <WorkImage
+              imgStyle={{ objectPosition: 'top' }}
+              fluid={image.node.childImageSharp.fluid}
+            />
+          </a>
+        );
+      })}
     </WorkItem>
   );
 };

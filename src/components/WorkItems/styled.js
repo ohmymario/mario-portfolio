9069b9cfa7748e7b2573968f060f1616283ec9db
Mario <mariob360@gmail.com>
2021-10-06 18:54:27 -0700

    Add starter code for hovering over projects

--- src/components/WorkItems/styled.js
+++ src/components/WorkItems/styled.js
@@ -8,22 +8,60 @@ export const WorkItem = styled.div`
   width: 100%;
   gap: 10px;
 
-  a {
+  .work-container {
     width: calc(33.3333% - 20px);
     border: 1px solid var(--gray5);
     max-height: 200px;
+    position: relative;
 
     ${media.lessThan('large')`
-            width: calc(50% - 10px);
-        `}
+        width: calc(50% - 10px);
+      `}
 
     ${media.lessThan('medium')`
-            width: 100%;
-        `}
+        width: 100%;
+    `}
 
-    ${media.lessThan('small')`
-            width: 100%;
-        `}
+    a {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      display: block;
+      overflow: hidden;
+      &:after {
+        position: absolute;
+        content: '';
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 999;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        transition: all 0.5s;
+      }
+    }
+
+    .work-desc {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      opacity: 0;
+      z-index: 999;
+      transition: all 0.3s ease-out;
+      pointer-events: none;
+    }
+
+    &:hover .work-desc {
+      opacity: 1;
+    }
+
+    &:hover a:after {
+      opacity: 1;
+      background-image: linear-gradient(to top, #ca5f5e, #d68584);
+    }
   }
 `;
 

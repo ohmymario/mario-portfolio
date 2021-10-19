5372ea0ab1e1193e39f0e986009b041c4f0d7360
Mario <mariob360@gmail.com>
2021-10-19 12:50:42 -0700

    Update links for email and linkedin

--- src/components/Contact/styled.js
+++ src/components/Contact/styled.js
@@ -8,7 +8,7 @@ export const ContactWrapper = styled.div`
 
   ${media.lessThan('medium')`
         flex-wrap: wrap;
-    `}
+  `}
 `;
 
 export const InfosWrapper = styled.div`
@@ -16,7 +16,7 @@ export const InfosWrapper = styled.div`
 
   ${media.lessThan('medium')`
         flex-basis: auto;
-    `}
+  `}
 
   h3 {
     margin-bottom: 12px;
@@ -24,6 +24,20 @@ export const InfosWrapper = styled.div`
 
   p {
     margin-bottom: 32px;
+  }
+
+  .contact-links {
+    margin-bottom: 10px;
+    margin-top: 5px;
+  }
+
+  a {
+    color: var(--gray6);
+    text-decoration: underline;
+    transition: all 0.25s;
+    &:hover {
+      color: var(--gray8);
+    }
   }
 `;
 
@@ -36,7 +50,6 @@ export const ImageWrapper = styled.div`
 
     ${media.lessThan('large')`
             width: 150px;
-
-        `}
+    `}
   }
 `;

b3951ef991f7f259eb4417078b1bc15143bfc761
Mario <mariob360@gmail.com>
2021-10-19 17:13:02 -0700

    Add netlify form stuff to test

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,14 +7,46 @@ import ContactImage from '../../images/contact/contact.png';
 
 import { ContactWrapper, ImageWrapper, InfosWrapper } from './styled';
 
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+}
+
 const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [bot, setBot] = useState();
+
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+    setBot();
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...name,
+        ...email,
+        ...message,
+        ...bot,
+      }),
+    })
+      .then(() => {
+        resetForm();
+      })
+      .catch((error) => alert(error));
   };
+
+  // .then(() => navigate(form.getAttribute('action')))
 
   return (
     <>
@@ -22,58 +54,89 @@ const Contact = () => {
         <Content>
           <ContactWrapper>
             <InfosWrapper>
-              <h1>Let's Talk</h1>
-              <p>
-                Let's get in touch, for any job opportunities or even simply say
-                hi, If you are feeling really adventurous, just drop me a line.
-              </p>
-
-              <p className="contact-links">
-                You can connect with me via{' '}
-                <a href="https://www.linkedin.com/in/mario-b-062470173/">
-                  Linkedin.
-                </a>
-              </p>
-
-              <p className="contact-links">
-                If you would rather email{' '}
-                <a href="mailto:mariob360@gmail.com">mariob360@gmail.com</a>
-              </p>
-
-              <form onSubmit={handleSubmit}>
+              <div className="contact-message">
+                <h1>Let's Talk</h1>
                 <p>
-                  <label htmlFor="text">
+                  Let's get in touch, for any job opportunities or even simply
+                  say hi, if you are feeling really adventurous, just drop me a
+                  line.
+                </p>
+              </div>
+
+              <div className="contact-links">
+                <p className="contact-link">
+                  You can connect with me via{' '}
+                  <a href="https://www.linkedin.com/in/mario-b-062470173/">
+                    Linkedin.
+                  </a>
+                </p>
+                <p className="contact-link">
+                  If you would rather email{' '}
+                  <a href="mailto:mariob360@gmail.com">mariob360@gmail.com</a>
+                </p>
+              </div>
+
+              <form
+                name="contact"
+                method="post"
+                action="/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                {/* HONEY POT */}
+                <input type="hidden" name="form-name" value="contact" />
+                <p hidden>
+                  <label htmlFor="bot-field">
+                    Don’t fill this out:{' '}
+                    <input
+                      name="bot-field"
+                      value={bot}
+                      onChange={(e) => setBot(e.target.value)}
+                    />
+                  </label>
+                </p>
+                {/* HONEY POT */}
+
+                <p className="form-name">
+                  <label htmlFor="name">
                     Your Name:{' '}
                     <input
-                      type="text"
                       onChange={(e) => setName(e.target.value)}
                       value={name}
                       name="name"
+                      type="text"
+                      required
                     />
                   </label>
                 </p>
-                <p>
+
+                <p className="form-email">
                   <label htmlFor="email">
                     Your Email:{' '}
                     <input
-                      type="email"
                       onChange={(e) => setEmail(e.target.value)}
                       value={email}
                       name="email"
+                      type="email"
                     />
                   </label>
                 </p>
-                <p>
+
+                <p className="form-message">
                   <label htmlFor="message">
                     Message:{' '}
                     <textarea
-                      name="message"
                       onChange={(e) => setMessage(e.target.value)}
                       value={message}
+                      name="message"
+                      required
                     />
                   </label>
                 </p>
-                <p>
+
+                <p className="form-submit">
                   <button type="submit">Send</button>
                 </p>
               </form>
--- src/components/Contact/styled.js
+++ src/components/Contact/styled.js
@@ -18,25 +18,31 @@ export const InfosWrapper = styled.div`
         flex-basis: auto;
   `}
 
-  h3 {
-    margin-bottom: 12px;
-  }
+  .contact-message {
+    h1 {
+      font-size: 46px;
+      margin-bottom: 12px;
+    }
 
-  p {
-    margin-bottom: 32px;
+    p {
+      /* max-width: 560px; */
+      font-size: 25px;
+      margin-bottom: 32px;
+    }
   }
 
   .contact-links {
-    margin-bottom: 10px;
-    margin-top: 5px;
-  }
-
-  a {
-    color: var(--gray6);
-    text-decoration: underline;
-    transition: all 0.25s;
-    &:hover {
-      color: var(--gray8);
+    .contact-link {
+      margin-bottom: 10px;
+      margin-top: 5px;
+      a {
+        color: var(--gray6);
+        text-decoration: underline;
+        transition: all 0.25s;
+        &:hover {
+          color: var(--gray8);
+        }
+      }
     }
   }
 `;

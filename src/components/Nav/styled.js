27cbd626c4a104ca8b19e6276b57f6f11c95731a
Mario <mariob360@gmail.com>
2021-11-09 16:31:35 -0800

    Improve size of nav buttons help mobile nav usage

--- src/components/Nav/styled.js
+++ src/components/Nav/styled.js
@@ -5,7 +5,7 @@ export const Wrapper = styled.nav`
   ul {
     position: relative;
     display: flex;
-    padding: 0 40px;
+    padding: 0;
     width: 100%;
     flex-wrap: wrap;
     list-style: none;
@@ -13,9 +13,12 @@ export const Wrapper = styled.nav`
 
     li {
       &:not(:last-child) {
-        margin-right: 48px;
+        margin-right: 2px;
       }
       a {
+        padding: 0 15px 0 15px;
+        width: 100%;
+        text-align: center;
         display: flex;
         align-items: center;
         font-size: 20px;
@@ -39,10 +42,8 @@ export const Wrapper = styled.nav`
 
   ${media.lessThan('large')`
        ul {
-        padding: 0 20px;
         li {
           &:not(:last-child) {
-            margin-right: 18px;
           }
            a {
                 font-size: 18px;

12f84a58d6ca4bb942c33b6a63d5bc73f3e78067
Mario <mariob360@gmail.com>
2021-11-18 17:54:13 -0800

    Add wrapper to 404 component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
+
+import Wrapper from '../components/Common/Wrapper';
+import Content from '../components/Common/Content';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
+    <Wrapper>
+      <Content>
+        <h2>Looks like there's nothing here. Let's go back home</h2>
+      </Content>
+    </Wrapper>
   </Layout>
 );
 

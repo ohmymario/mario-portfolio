7812cc3329c278dce6ef7ca61bf7c41ad941bf56
Mario <mariob360@gmail.com>
2021-12-02 13:32:33 -0800

    create new notfound component and start moving

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import AniLink from 'gatsby-plugin-transition-link/AniLink';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
 import Wrapper from '../components/Common/Wrapper';
 import Content from '../components/Common/Content';
-
+import NotFound from '../components/NotFound/index';
+// MOVING MOST OF THIS TO NTOFOUND
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <Wrapper>
       <Content>
-        <h2>Looks like there's nothing here. Let's go back home</h2>
+        <NotFound />
       </Content>
     </Wrapper>
   </Layout>

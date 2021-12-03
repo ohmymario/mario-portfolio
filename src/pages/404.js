d00264c47dcb6440332c5103954bea9f898d9705
Mario <mariob360@gmail.com>
2021-12-03 13:16:59 -0800

    remove comment

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import SEO from '../components/seo';
 import Wrapper from '../components/Common/Wrapper';
 import Content from '../components/Common/Content';
 import NotFound from '../components/NotFound/index';
-// MOVING MOST OF THIS TO NTOFOUND
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />

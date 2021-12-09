c99a7a73b8441d5d59e37f83b84b14e6662fdf37
Mario <mariob360@gmail.com>
2021-12-09 13:37:52 -0800

    add madewithgatsby styles

---
+++ src/components/MadeWithGatsby/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+export const MadeWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 25px;
+  font-size: 12px;
+  p {
+    font-size: 1em;
+    color: white;
+    margin: 0;
+  }
+  img {
+    vertical-align: top;
+    height: 1.825em;
+    padding-right: 3px;
+  }
+`;

852e922aabd0c483bfe63b7772e74dc039fa8f9c
Mario Ballesteros <mariob360@gmail.com>
2024-06-06 13:41:51 -0700

    feat(worksData): add slugs to all data points
    - This change is a preparatory step for dynamically generating pages based on the data.
    - Each data point now includes a unique slug.
    - Updated related functions to handle the new slug property.

--- src/components/WorkItems/worksData.js
+++ src/components/WorkItems/worksData.js
@@ -1,36 +1,42 @@
 const portfolioData = [
   {
     name: `coffeeroasters`,
+    slug: 'coffee-roasters',
     url: 'https://coffee-roasters.vercel.app/',
     tech: 'NextJS, React, Styled-Components',
-    image: 'coffeeroasters.png',
+    image: 'coffee-roasters.png',
   },
   {
     name: `React Colors`,
+    slug: 'react-colors',
     url: 'https://laughing-boyd-cdf70d.netlify.app/',
     tech: 'React, React-Router, Material-UI',
     image: 'react-colors.png',
   },
   {
     name: `Design Portfolio`,
+    slug: 'design-portfolio',
     url: 'https://amy-design-portfolio.netlify.app',
     tech: 'TypeScript, React, Styled-Components, CSS Variables',
     image: 'design-portfolio.png',
   },
   {
     name: `Slick's Slices`,
+    slug: 'slicks-slices',
     url: 'https://slicks-slicess.netlify.app/',
     tech: 'Gatsby, Sanity, Styled-Components, GraphQL, CSS Variables',
     image: 'slick-slices.png',
   },
   {
     name: `Weatherman`,
+    slug: 'weatherman',
     url: 'https://weatherman-demo.netlify.app/',
     tech: 'OpenWeatherMap API, React, Font-Awesome, Styled-Components',
     image: 'weatherman.png',
   },
   {
     name: `React Netflix Clone`,
+    slug: 'react-netflix-clone',
     url: 'https://firebase-netflix-clone.netlify.app/',
     tech: 'Firebase, React, FuseJS, React-Router',
     image: 'netflix-clone.png',
